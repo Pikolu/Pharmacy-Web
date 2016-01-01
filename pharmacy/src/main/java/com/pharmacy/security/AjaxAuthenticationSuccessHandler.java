306450ea928f286543a8a39e7a85c48e0f982851
Alexander Popow <apopow@gmail.com>
2016-01-01 20:42:17 +0100

    add

--- pharmacy/src/main/java/com/pharmacy/security/AjaxAuthenticationSuccessHandler.java
+++ pharmacy/src/main/java/com/pharmacy/security/AjaxAuthenticationSuccessHandler.java
@@ -1,6 +1,8 @@
 package com.pharmacy.security;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -18,5 +20,8 @@ public class AjaxAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuc
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         response.setStatus(HttpServletResponse.SC_OK);
+        RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+        redirectStrategy.sendRedirect(request, response, "index");
+        setRedirectStrategy(redirectStrategy);
     }
 }
--- pharmacy/src/main/java/com/pharmacy/web/IndexController.java
+++ pharmacy/src/main/java/com/pharmacy/web/IndexController.java
@@ -1,6 +1,8 @@
 package com.pharmacy.web;
 
+import com.pharmacy.config.SecurityUtils;
 import com.pharmacy.domain.User;
+import com.pharmacy.security.CustomUserDetails;
 import com.pharmacy.service.api.ImportService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +11,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,14 +32,7 @@ public class IndexController {
 
     @RequestMapping(value = {"/", "/index", "/welcome**"}, method = RequestMethod.GET)
     public ModelAndView index() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        ModelAndView modelAndView = new ModelAndView("index");
-        if (authentication instanceof User) {
-            LOG.info("User is authenticated => {}", authentication);
-        } else {
-            LOG.info("User is not authenticated => {}", authentication);
-        }
-        return modelAndView;
+        return new ModelAndView("index");
     }
 
 
@@ -44,6 +40,11 @@ public class IndexController {
     public void importTest(Model model) {
         Assert.notNull (model);
         importService.importCSVFile();
+    }
+
+    @ModelAttribute("customUser")
+    public CustomUserDetails getCustomUserDetails() {
+        return SecurityUtils.getCurrentUser();
     }
 
 }
